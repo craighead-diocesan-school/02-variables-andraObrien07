62bbebe2abd4c58e1f925a779f094335664095c9
andraObrien07 <comanan09@craighead.school.nz>
2023-06-01 10:11:40 +1200

    done

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -4,6 +4,8 @@
 
 function pupperAge() {
   alert('Dog years are human years times seven.')
-
+  let dog = 7
   let humanYears = prompt('How old is your dog in human years?')
+  let result = humanYears * dog
+  alert('Your dog is ' + result + ' years old.')
 }
--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,7 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+  let left = prompt('How many lessons do you have left today?')
+  let seconds = 60
+  alert('Thats ' + left * seconds + ' seconds!')
 }
