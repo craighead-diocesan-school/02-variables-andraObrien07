746d277f6d40590430d6476686c80ad58b42521e
andraObrien07 <comanan09@craighead.school.nz>
2023-06-01 10:17:04 +1200

    YASSSSS

--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,9 @@
 // ####################################
 
 function lifeRemaining() {
-  
+    let age = prompt('How old are you?')
+    let proper = 83.5
+    let real = 83.5 - age
+    alert("You've got " + real + " years let in you!")
+
 }
